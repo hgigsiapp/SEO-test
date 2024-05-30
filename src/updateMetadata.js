b3cdd8f3102d96c92884c0a5f0778240c53486ba
Haven Giguere <hgiguere@iapp.org>
2024-05-30 16:17:56 -0400

    hi

--- src/updateMetadata.js
+++ src/updateMetadata.js
@@ -2,28 +2,20 @@ import React from 'react';
 import { Helmet } from 'react-helmet-async';
 
 // Function to update metadata using React Helmet
-const Element = async (newDescription) => {
-  const url = 'https://google-translate1.p.rapidapi.com/language/translate/v2/detect';
-const options = {
-	method: 'POST',
-	headers: {
-		'x-rapidapi-key': 'Sign Up for Key',
-		'x-rapidapi-host': 'google-translate1.p.rapidapi.com',
-		'Content-Type': 'application/json',
-		'Accept-Encoding': 'application/gzip'
-	},
-	body: {
-		q: 'English is hard, but detectably so'
-	}
-};
-
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
+const Element =  (newDescription) => {
+  fetch('https://api.chucknorris.io/jokes/random')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data.value); // This will log the Chuck Norris joke
+  })
+  .catch(error => {
+    console.error('There was a problem with the fetch operation:', error);
+  });
   return (
     <div>
     <Helmet>
