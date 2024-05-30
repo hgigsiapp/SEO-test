8a31580b42c31a9f2c92fcad421cc79534f450bc
Haven Giguere <hgiguere@iapp.org>
2024-05-30 15:53:31 -0400

    hi

--- src/updateMetadata.js
+++ src/updateMetadata.js
@@ -5,20 +5,7 @@ import { Helmet } from 'react-helmet-async';
 
 // Function to update metadata using React Helmet
 const Element = async (newDescription) => {
-  const myHeaders = new Headers();
-myHeaders.append("Accept", "application/json");
-
-const requestOptions = {
-  method: "GET",
-  headers: myHeaders,
-  redirect: "follow"
-};
-
-  fetch("https://icanhazdadjoke.com/", requestOptions)
-    .then((response) =>  response.text())
-    .then((result) => console.log(result))
-
-  // console.log("this is the result", result)
+  
   return (
     <div>
     <Helmet>
