62c8c091fcfe735100967cb3586765c94de7f92d
Haven Giguere <hgiguere@iapp.org>
2024-05-30 10:34:17 -0400

    hi

--- public/updateMetadata.js
+++ public/updateMetadata.js
@@ -1,2 +1,9 @@
-let descriptionMetaTag = document.querySelector('meta[name="description"]');
-descriptionMetaTag.setAttribute('content', 'this is a new description');
+// Create a new meta tag element
+let newMetaTag = document.createElement('meta');
+
+// Set attributes for the new meta tag
+newMetaTag.setAttribute('name', 'description');
+newMetaTag.setAttribute('content', 'The new description');
+
+// Append the new meta tag to the head section of the document
+document.head.appendChild(newMetaTag);
