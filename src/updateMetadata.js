de101cd021ec87ba4f58e3364d6cb559867d24f7
Haven Giguere <hgiguere@iapp.org>
2024-05-30 16:20:53 -0400

    hi

--- src/updateMetadata.js
+++ src/updateMetadata.js
@@ -1,27 +1,31 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
 
-// Function to update metadata using React Helmet
-const Element =  (newDescription) => {
-  fetch('https://api.chucknorris.io/jokes/random')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  })
-  .then(data => {
-    console.log(data.value); // This will log the Chuck Norris joke
-  })
-  .catch(error => {
-    console.error('There was a problem with the fetch operation:', error);
-  });
+const Element = () => {
+  const [joke, setJoke] = useState('');
+
+  useEffect(() => {
+    fetch('https://api.chucknorris.io/jokes/random')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        setJoke(data.value); 
+      })
+      .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+      });
+  }, []);
+
   return (
     <div>
-    <Helmet>
-      <meta name="description" content="the new element description" />
-    </Helmet>
-    <div>Hi How Ah Ya!</div>
+      <Helmet>
+        <meta name="description" content={joke || "the new element description"} />
+      </Helmet>
+      <div>Hi How Ah Ya!</div>
     </div>
   );
 };
