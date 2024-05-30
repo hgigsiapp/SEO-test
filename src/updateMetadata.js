0181c3f23128832892d77c2b01902be903fb8891
Haven Giguere <hgiguere@iapp.org>
2024-05-30 15:08:55 -0400

    hi

--- src/updateMetadata.js
+++ src/updateMetadata.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
 
+
+
 // Function to update metadata using React Helmet
-const Element = (newDescription) => {
+const Element = async (newDescription) => {
+  const url = 'https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly?lat=35.5&lon=-78.5';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+		'X-RapidAPI-Host': 'weatherbit-v1-mashape.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
   return (
     <div>
     <Helmet>
