ca16a3c65a98fdc2e1f04a7e95628d0e1b0e89c2
Haven Giguere <hgiguere@iapp.org>
2024-05-31 09:41:21 -0400

    ghi

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Element from './updateMetadata'
-// import ReactDOM from 'react-dom';
+
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 function App() {
@@ -9,18 +9,15 @@ function App() {
     <HelmetProvider>
     <div className="App">
       <Helmet>
-                <title>My Title</title>
-                <meta name="description" content="Helmet application" />
-                <meta property="og:title" content="Facebook Friendly title" />
-                <meta property="og:description" content="hi facebook" />
-            </Helmet>
-            <Element />
+        <title>My Title</title>
+        <meta name="description" content="Helmet application" />
+        <meta property="og:title" content="Facebook Friendly title" />
+        <meta property="og:description" content="hi facebook" />
+      </Helmet>
+      <Element />
     </div>
     </HelmetProvider>
   );
 }
-// ReactDOM.hydrate(
-//   app,
-//   document.getElementById(‘app’)
-// );
+
 export default App;
---
+++ src/Body.js
@@ -0,0 +1,9 @@
+const Body = () => {
+    return (
+        <div>
+            <img src="../public/images/Ai-gov.png"></img>
+        </div>
+    )
+}
+
+export default Body;
