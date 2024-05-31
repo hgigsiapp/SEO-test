bc3bd0f32095c15f63ac9afba3ca7f240aba87b8
Haven Giguere <hgiguere@iapp.org>
2024-05-31 09:48:43 -0400

    ghi

--- src/Body.js
+++ src/Body.js
@@ -1,9 +1,13 @@
+import React from 'react';
+import aiGovImage from '../public/images/Ai-gov.png';
+
 const Body = () => {
     return (
         <div>
-            <img src="../public/images/Ai-gov.png"></img>
+            <h1>My Component</h1>
+            <img src={aiGovImage} alt="AI Gov" />
         </div>
-    )
+    );
 }
 
 export default Body;
