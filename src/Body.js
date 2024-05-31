148584f8205d79c59fc2a85f78804784a664c67d
Haven Giguere <hgiguere@iapp.org>
2024-05-31 10:00:15 -0400

    ghi

--- src/Body.js
+++ src/Body.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import aiGovImage from './images/Ai-gov.png';
+import image1 from './images/Ai-gov.png';
+import image2 from './images/Iapp-news.png'
+import image3 from './images/american_privacy_rights_act_cheat_sheet_thumbnail.jpg'
+import image4 from './images/ftc_enforcement_trends_thumbnail.jpg'
 
 const Body = () => {
     return (
         <div>
-            <h1>My Component</h1>
-            <img src={aiGovImage} alt="AI Gov" />
+            <p>Hello</p>
+            <img src={image1} alt="AI Gov" />
+            <p>Hola</p>
+            <img src={image2} alt="AI Gov" />
+            <p>Bonjour</p>
+            <img src={image3} alt="AI Gov" />
+            <p>Ciao</p>
+            <img src={image4} alt="AI Gov" />
         </div>
     );
 }
