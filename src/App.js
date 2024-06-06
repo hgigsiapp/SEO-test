44ad751b3a574d2251bb394f7513eda29ff17de3
Haven Giguere <hgiguere@iapp.org>
2024-06-06 10:09:39 -0400

    adds react-router

--- src/App.js
+++ src/App.js
@@ -1,36 +1,36 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
-import Element from './updateMetadata'
-import Body from './Body'
-
-import {Helmet} from "react-helmet";
+import Element from './updateMetadata';
+import Body from './Body';
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
-    <div className="App">
-      <Helmet>
-        <title>My Title</title>
-        <meta name="description" content="Helmet application" />
-        <meta property="og:title" content="Facebook Friendly title" />
-        <meta property="og:description" content="hi facebook" />
-        <meta
-        property="og:url"
-        content="hgigsiapp.github.io/SEO-test/"
-      />
-      <meta
-        property="og:image"
-        content="https://hgigsiapp.github.io/SEO-test/images/Ai-gov.png"
-      />
-      <meta property="twitter:title" content="twitter Friendly title" />
-        <meta property="twitter:description" content="hi twitter" />
-        <meta
-        property="twitter:url"
-        content="hgigsiapp.github.io/SEO-test/"
-      />
-      </Helmet>
-      <Element />
-      <Body />
-    </div>
+    <Router>
+      <div className="App">
+        <Helmet>
+          <title>My Title</title>
+          <meta name="description" content="Helmet application" />
+          <meta property="og:title" content="Facebook Friendly title" />
+          <meta property="og:description" content="hi facebook" />
+          <meta property="og:url" content="hgigsiapp.github.io/SEO-test/" />
+          <meta
+            property="og:image"
+            content="https://hgigsiapp.github.io/SEO-test/images/Ai-gov.png"
+          />
+          <meta property="twitter:title" content="twitter Friendly title" />
+          <meta property="twitter:description" content="hi twitter" />
+          <meta
+            property="twitter:url"
+            content="hgigsiapp.github.io/SEO-test/"
+          />
+        </Helmet>
+        <Route path="/" component={Element} />
+        <Route path="/body" component={Body} />
+      </div>
+    </Router>
   );
 }
 
--- src/Body.js
+++ src/Body.js
@@ -5,8 +5,28 @@ import image3 from './images/american_privacy_rights_act_cheat_sheet_thumbnail.j
 import image4 from './images/ftc_enforcement_trends_thumbnail.jpg'
 
 const Body = () => {
+    const [joke, setJoke] = useState('');
+
+  useEffect(() => {
+    fetch('https://api.chucknorris.io/jokes/random')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        setJoke(data.value); 
+      })
+      .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+      });
+  }, []);
     return (
         <div>
+            <Helmet>
+        <meta name="description" content={joke || "the new body description"} />
+      </Helmet>
             <p>Hello</p>
             <img src={image1} alt="AI Gov" />
             <p>Hola</p>
